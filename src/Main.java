ef4e5ca19c2f789655926d2f891b13f1601cedae
f h <falhamad2002@gmail.com>
2024-11-06 13:49:28 +0300

    first bonus is complete

--- src/Main.java
+++ src/Main.java
@@ -10,24 +10,24 @@ public class Main {
         String validUsername = "fh292";
         String validPassword = "fatma123";
 
-        //Implement Try-Catch for Input and Validation:
-        try{
-            System.out.print("Enter your username: ");
-            String username = scanner.nextLine();
-            System.out.print("Enter your password: ");
-            String password = scanner.nextLine();
-            if (!username.equals(validUsername)||!password.equals(validPassword)) {
-                throw new Exception("Invalid credentials");
+        //Introduce a Loop for Multiple Attempts:
+        while (true) {
+            //Implement Try-Catch for Input and Validation:
+            try {
+                System.out.print("Enter your username: ");
+                String username = scanner.nextLine();
+                System.out.print("Enter your password: ");
+                String password = scanner.nextLine();
+                if (!username.equals(validUsername) || !password.equals(validPassword)) {
+                    throw new Exception("Invalid credentials");
+                } else {
+                    System.out.println("You are logged in");
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println("Incorrect username and/or password.");
             }
-            else{
-                System.out.println("You are logged in");
-            }
-
-        } catch (Exception e) {
-            System.out.println("Incorrect username and/or password.");
         }
-
-
         //Close the Scanner:
         scanner.close();
     }
