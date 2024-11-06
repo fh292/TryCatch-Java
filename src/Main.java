79ad3abed69cdda23c3e89170c28371177a33062
f h <falhamad2002@gmail.com>
2024-11-06 14:57:11 +0300

    second bonus is done

--- src/Main.java
+++ src/Main.java
@@ -10,24 +10,51 @@ public class Main {
         String validUsername = "fh292";
         String validPassword = "fatma123";
 
-        //Introduce a Loop for Multiple Attempts:
-        while (true) {
-            //Implement Try-Catch for Input and Validation:
+        // Implement Attempt Counting
+        for (int attempt = 1; attempt <= 5; attempt++) {
             try {
+                System.out.println("Attempt #" + attempt);
                 System.out.print("Enter your username: ");
                 String username = scanner.nextLine();
                 System.out.print("Enter your password: ");
                 String password = scanner.nextLine();
+
                 if (!username.equals(validUsername) || !password.equals(validPassword)) {
-                    throw new Exception("Invalid credentials");
+                    System.out.println("Incorrect username and/or password.");
+                    System.out.println("\n");
                 } else {
                     System.out.println("You are logged in");
-                    break;
+                    break;                }
+
+                //Throw an Error After Maximum Attempts:
+                if (attempt == 5) {
+                    throw new Exception("Maximum attempts exceeded");
                 }
+
             } catch (Exception e) {
-                System.out.println("Incorrect username and/or password.");
+                System.out.println("Maximum attempts exceeded");
             }
         }
+//
+//        //Introduce a Loop for Multiple Attempts:
+//        while (true) {
+//            //Implement Try-Catch for Input and Validation:
+//            try {
+//                System.out.print("Enter your username: ");
+//                String username = scanner.nextLine();
+//                System.out.print("Enter your password: ");
+//                String password = scanner.nextLine();
+//                if (!username.equals(validUsername) || !password.equals(validPassword)) {
+//                    throw new Exception("Invalid credentials");
+//                } else {
+//                    System.out.println("You are logged in");
+//                    break;
+//                }
+//            } catch (Exception e) {
+//                System.out.println("Incorrect username and/or password.");
+//            }
+//        }
+
         //Close the Scanner:
         scanner.close();
     }
